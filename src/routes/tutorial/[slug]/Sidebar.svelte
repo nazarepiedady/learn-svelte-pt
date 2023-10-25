2bfb2faa05334e4d757da63167f6331ed358d403
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-10-25 06:22:02 +0100

    docs(pt): translate `routes/tutorial/[slug]/sidebar.svelte`

--- src/routes/tutorial/[slug]/Sidebar.svelte
+++ src/routes/tutorial/[slug]/Sidebar.svelte
@@ -75,7 +75,7 @@
 		</div>
 
 		{#if exercise.next}
-			<p><a href="/tutorial/{exercise.next.slug}">Next: {exercise.next.title}</a></p>
+			<p><a href="/tutorial/{exercise.next.slug}">Próximo: {exercise.next.title}</a></p>
 		{/if}
 	</div>
 
@@ -84,9 +84,9 @@
 			target="_blank"
 			rel="noreferrer"
 			class="edit"
-			href="https://github.com/sveltejs/learn.svelte.dev/tree/main/{exercise.dir}"
+			href="https://github.com/nazarepiedady/learn-svelte-pt/tree/main/{exercise.dir}"
 		>
-			Edit this page
+			Editar esta página
 		</a>
 	</footer>
 </section>
@@ -94,11 +94,10 @@
 {#if show_modal}
 	<Modal on:close={() => (show_modal = false)}>
 		<div class="modal-contents">
-			<h2>Copy and paste is currently disabled!</h2>
+			<h2>Copiar e colar está atualmente desligado!</h2>
 
 			<p>
-				We recommend typing the code into the editor to complete the exercise, as this results in
-				better retention and understanding.
+				Nós recomendados digitar o código no editor para completar o exercício, uma vez que isto resulta na melhor retenção e entendimento.
 			</p>
 			<label>
 				<input
@@ -107,7 +106,7 @@
 						sessionStorage[copy_enabled] = e.currentTarget.checked ? 'true' : '';
 					}}
 				/>
-				enable copy-and-paste for the duration of this session
+				ativar copiar e colar para a duração desta sessão
 			</label>
 
 			<button on:click={() => (show_modal = false)}>OK</button>
