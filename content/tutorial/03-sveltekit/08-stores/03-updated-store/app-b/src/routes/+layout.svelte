6e02c771ea2aad95ed80546c35c2ea601c917d36
Nazaré da Piedade <nazarepiedady@gmail.com>
2024-01-03 22:38:37 +0100

    docs(en): add untranslated `sveltekit/stores/updated-store/app-b`

---
+++ content/tutorial/03-sveltekit/08-stores/03-updated-store/app-b/src/routes/+layout.svelte
@@ -0,0 +1,54 @@
+<script>
+	import { page, navigating, updated } from '$app/stores';
+</script>
+
+<nav>
+	<a href="/" aria-current={$page.url.pathname === '/'}>
+		home
+	</a>
+
+	<a href="/about" aria-current={$page.url.pathname === '/about'}>
+		about
+	</a>
+
+	{#if $navigating}
+		navigating to {$navigating.to.url.pathname}
+	{/if}
+</nav>
+
+<slot />
+
+{#if $updated}
+	<div class="toast">
+		<p>
+			A new version of the app is available
+
+			<button on:click={() => location.reload()}>
+				reload the page
+			</button>
+		</p>
+	</div>
+{/if}
+
+<style>
+	.toast {
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		padding: 1rem;
+		display: flex;
+		justify-content: center;
+		gap: 1rem;
+	}
+
+	.toast p {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		background: var(--bg-2);
+		padding: 0.5rem 0.5rem 0.5rem 1rem;
+		border-radius: 4px;
+	}
+</style>
+
