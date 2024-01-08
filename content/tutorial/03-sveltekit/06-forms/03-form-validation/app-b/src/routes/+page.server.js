d804aaecc0ac5bd73f8c7b0551f240760971944e
Nazaré da Piedade <nazarepiedady@gmail.com>
2024-01-08 15:07:04 +0100

    docs(en): update `forms/form-validation/app-b/src/routes/+page.server.js`

--- content/tutorial/03-sveltekit/06-forms/03-form-validation/app-b/src/routes/+page.server.js
+++ content/tutorial/03-sveltekit/06-forms/03-form-validation/app-b/src/routes/+page.server.js
@@ -2,14 +2,15 @@ import { fail } from '@sveltejs/kit';
 import * as db from '$lib/server/database.js';
 
 export function load({ cookies }) {
-	const id = cookies.get('userid');
+	let id = cookies.get('userid');
 
 	if (!id) {
-		cookies.set('userid', crypto.randomUUID(), { path: '/' });
+		id = crypto.randomUUID();
+		cookies.set('userid', id, { path: '/' });
 	}
 
 	return {
-		todos: db.getTodos(id) ?? []
+		todos: db.getTodos(id)
 	};
 }
 
