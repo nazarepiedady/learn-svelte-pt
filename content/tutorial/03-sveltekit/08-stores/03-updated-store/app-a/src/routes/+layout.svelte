060283483619a55187cfd5472e71cece3c8ee379
Nazaré da Piedade <nazarepiedady@gmail.com>
2024-01-11 06:21:24 +0100

    docs(en): update `sveltekit/stores/updated-store/app-a/src/routes/+layout.svelte`

--- content/tutorial/03-sveltekit/08-stores/03-updated-store/app-a/src/routes/+layout.svelte
+++ content/tutorial/03-sveltekit/08-stores/03-updated-store/app-a/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { page, navigating, updated } from '$app/stores';
+	import { page, navigating } from '$app/stores';
 </script>
 
 <nav>
@@ -18,12 +18,35 @@
 
 <slot />
 
-{#if $updated}
-	<p class="toast">
+<div class="toast">
+	<p>
 		A new version of the app is available
 
 		<button on:click={() => location.reload()}>
 			reload the page
 		</button>
 	</p>
-{/if}
+</div>
+
+<style>
+	.toast {
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		padding: 1rem;
+		display: flex;
+		justify-content: center;
+		gap: 1rem;
+	}
+
+	.toast p {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		background: var(--bg-2);
+		padding: 0.5rem 0.5rem 0.5rem 1rem;
+		border-radius: 4px;
+	}
+</style>
+
